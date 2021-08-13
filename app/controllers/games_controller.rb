262c7ebbc1066cc9d2cea5c3a0f0f55a5411fe11
Ozgur Firat Sayin <ozgurfiratsayin@gmail.com>
2021-08-13 18:09:16 +0100

    Fix the guess to lettes compare logic, add replay link, wrap the pages with container

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,36 +1,31 @@
 require 'open-uri'
 
 class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
   def new
-    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+    @letters = Array.new(4) { VOWELS.sample }
+    @letters += Array.new(6) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
   end
 
   def score
     @guess = params[:guess];
     @letters = params[:letters];
+    @included = included?(@guess, @letters)
+    @english_word = english_word?(@guess)
+    @score = @guess.length
+  end
 
+  private
 
-    def included?(guess, letters)
-        guess.chars.map {|e| e.upcase}.all? { |letter| guess&.count(letter) <= letters.count(letter) }
-    end
+  def included?(guess, letters)
+    guess = guess.chars.map(&:upcase)
+    guess.all? { |letter| guess.count(letter) <= letters.count(letter) }
+  end
 
-    def english_word?(word)
-      response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
-      json = JSON.parse(response.read)
-      return json['found']
-    end
-
-    def compute_score(guess)
-      guess.length
-    end
-
-    if included?(@guess, @letters) === false
-      @result = "Sorry but #{@guess} cannot be built out of #{@letters}"
-    elsif english_word?(@guess) === false
-      @result = "Sorry but #{@guess} does not seem to be an English word"
-    else
-      @result = "Congratulations #{@guess} is a valid English word and your score is #{compute_score(@guess)}"
-    end
-    return @letters;
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
   end
 end
